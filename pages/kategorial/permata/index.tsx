ee416fc4e55f54f9c00888116b52f6c518ca45f9
Gilberdi Sinaga <gilberdising012@gmail.com>
2023-03-28 23:01:15 +0700

    fix: error in deployment

--- pages/kategorial/permata/index.tsx
+++ pages/kategorial/permata/index.tsx
@@ -4,7 +4,6 @@ import { createClient } from "@/prismicio";
 function Permata({ results, cover }: any) {
   return (
     <>
-      <Kategorial linkTo="/kategorial/permata" title="Permata" kegiatan={results} cover={cover.results[0].data.moria.url} path='/kategorial/permata/kegiatan-info' />
     </>
   )
 }
