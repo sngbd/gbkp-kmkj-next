2f4f30d9cece229fc6f95d798b9e46ab8d860672
Gilberdi Sinaga <gilberdising012@gmail.com>
2023-06-04 00:25:35 +0800

    feat: make homepage responsive

--- components/Info.tsx
+++ components/Info.tsx
@@ -5,41 +5,37 @@ const Info = () => {
     <>
     <div className={styles.container}>
       <div className={styles.infos}>
-        <div className={styles.info}>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Runggun</div>
-            <div className={styles.number}>20</div>
-          </div>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Perp</div>
-            <div className={styles.number}>20</div>
-          </div>
-          <div className={styles.statistic}>
-            <div className={styles.title}>PJJ</div>
-            <div className={styles.number}>116</div>
-          </div>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Rumah Tangga</div>
-            <div className={styles.number}>3647</div>
-          </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Runggun</div>
+          <div className={styles.number}>20</div>
         </div>
-        <div className={styles.info}>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Anggota</div>
-            <div className={styles.number}>11847</div>
-          </div>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Personalia</div>
-            <div className={styles.number}>26</div>
-          </div>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Pertua - Diaken</div>
-            <div className={styles.number}>503</div>
-          </div>
-          <div className={styles.statistic}>
-            <div className={styles.title}>Tempat Kebaktian</div>
-            <div className={styles.number}>29</div>
-          </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Perp</div>
+          <div className={styles.number}>20</div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>PJJ</div>
+          <div className={styles.number}>116</div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Rumah Tangga</div>
+          <div className={styles.number}>3647</div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Anggota</div>
+          <div className={styles.number}>11847</div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Personalia</div>
+          <div className={styles.number}>26</div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Pertua - Diaken</div>
+          <div className={styles.number}>503</div>
+        </div>
+        <div className={styles.statistic}>
+          <div className={styles.title}>Tempat Kebaktian</div>
+          <div className={styles.number}>29</div>
         </div>
       </div>
     </div>
