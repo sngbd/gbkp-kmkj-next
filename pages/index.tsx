a4ee7632eed330c2efac5e908302ed20ea4ef276
Gilberdi Sinaga <gilberdising012@gmail.com>
2023-06-04 14:41:44 +0800

    feat: connect info with prismic

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ import Mingguan from '@/components/Mingguan';
 import Personalia from '@/components/Personalia';
 import Info from '@/components/Info';
 
-const Home = ({ berita, bpmk }: any) => {
+const Home = ({ berita, bpmk, info }: any) => {
   return ( 
     <>
       <Hero title="GBKP Klasis Medan Kutajurung" left="Hubungi Kami" right="Lokasi Kantor" />
-      <Info />
+      <Info results={info.results[0].data.info} />
       <Berita berita={berita.results.slice(0, 3)} />
       <Mingguan />
       <Personalia bpmk={bpmk.results[0].data.bpmk} />
@@ -22,9 +22,10 @@ export async function getServerSideProps() {
 
   const berita = await client.getByType('berita')
   const bpmk = await client.getByType('bpm')
+  const info = await client.getByType('info_statistik')
 
   return {
-    props: { berita, bpmk }
+    props: { berita, bpmk, info }
   }
 }
 
